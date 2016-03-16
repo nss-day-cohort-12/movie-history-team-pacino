98940bf9161dde12ed06fe2483b35dfac50f44e7
duggerNot <dugger.jk@gmail.com>
2016-03-16 15:30:40 -0500

    fixed a firebase thing and a route thing

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@
 // same thing applies to the $routeProvider word. these enable routing for views, not in vanilla angular
 let MovieApp = angular.module("MovieApp", ['ngRoute', 'firebase'])
   // define my personal firebase URL as a constant to be referenced later as firebaseURL
-  .constant('firebaseURL', "https://pizzapaperairplane.firebaseio.com/");
+  .constant('firebaseURL', "https://pizzapaperairplane.firebaseio.com/movies/.json");
 
 /*
   Define a promise for any view that needs an authenticated user
@@ -27,10 +27,6 @@ let isAuth = (authFactory) => new Promise((resolve, reject) => {
 MovieApp.config(["$routeProvider",
   function ($routeProvider) {
     $routeProvider.
-      when("/login", {
-        templateUrl: "partials/login.html",
-        controller: "MyMoviesCtrl"
-      }).
       when("/list", {
         templateUrl: "partials/my-movies-list.html",
         controller: "MyMoviesCtrl"
